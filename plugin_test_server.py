1e40413fda3e1df9109082e9758d87bde0ef680e
Javier Guerra <javier.guerra@konghq.com>
2021-05-25 15:13:16 -0500

    client method arguments and handling responses

--- plugin_test_server.py
+++ plugin_test_server.py
@@ -1,23 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import argparse
 import json
 import sys
 import logging
+import subprocess
+import os.path
+import time
+import socket
 
 from socketserver import ThreadingMixIn, UnixStreamServer, StreamRequestHandler
 
 import msgpack
 
 
+
+
+def wait_for_file(fname: str):
+	logging.info(f"waiting for {fname}")
+	while not os.path.exists(fname):
+		time.sleep(0.2)
+	logging.info("got it")
+
+
 class MpStream():
-	def __init__(self, socket):
-		self.socket = socket
+	def __init__(self, skt):
+		self.socket = skt
 		self.packer = msgpack.Packer()
-		self.unpacker = msgpack.Unpacker(socket)
+		self.unpacker = msgpack.Unpacker(self)
+
+	def read(self, size=-1):
+		if size > 0:
+			return self.socket.recv(size)
+		return self.socket.recv()
+
+	def write(self, b):
+		return self.socket.send(b)
 
 	def send(self, msg):
-		self.socket.write(self.packer.pack(msg))
+		self.write(self.packer.pack(msg))
 
 # ==== test servers ====
 
@@ -87,20 +108,41 @@ class MsgPackMockServer():
 
 class MsgPackTestClient():
 
-	def __init__(self, socket, try_methods):
+	def __init__(self, spec):
 		super().__init__()
-		self.socketname = socket
+		self.start_cmd = spec.get('start_cmd')
+		self.socketname = spec["socket"]
+		self.try_methods = spec.get("try_methods", [])
+
 		self.log_data = []
-		self.try_methods = try_methods
+
+	def start_target(self):
+		if not self.start_cmd:
+			return
+
+		logging.info(f"starting server: {self.start_cmd}")
+		self.proc = subprocess.Popen(
+			self.start_cmd,
+			stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		firstline = self.proc.stdout.readline()
+		logging.info(f">>>> {firstline}")
+
+	def close(self):
+		self.proc.terminate()
 
 	def try_method(self, m):
 		method, params = m
 		msg_id = 1
-		self.stream.send((0, msg_id, method, params))
+		msg = (0, msg_id, method, params)
+		logging.info(f"call method   : {method}({params})")
+		logging.debug(f"sending msg {msg}")
+		self.stream.send(msg)
 
 		for msg in self.stream.unpacker:
+			logging.debug(f"got msg: {msg}")
 			#  TODO: compare with some expected
 			if len(msg) == 3:
+				logging.info(f"notification  : {msg}")
 				self.log_data.append({"notification": msg})
 			elif len(msg) == 4:
 				r_type, r_msgid, r_err, r_result = msg
@@ -108,27 +150,32 @@ class MsgPackTestClient():
 					self.log_data.append({"response": msg})
 					if r_err:
 						logging.error(f"receive error: {r_err}")
+					logging.info(f"valid response: {r_result}")
 					return
 				raise AssertionError(f"unexpected response: {msg}")
 			else:
 				raise AssertionError(f"malformed message received: {msg}")
 
 	def try_all(self):
-		with open(self.socketname) as socket:
-			self.stream = MpStream(socket)
+		wait_for_file(self.socketname)
+		with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as skt:
+			skt.connect(self.socketname)
+			self.stream = MpStream(skt)
 			for i, m in enumerate(self.try_methods):
 				try:
 					self.try_method(m)
-				except Exception as e:
-					logging.error(f"trying method #{i}: {e}")
+					print(f"method # {i}", end="\r")
+				except Exception:
+					logging.error(f"trying method #{i}")
+			print("                 ")
 
 	@staticmethod
 	def start(spec):
 		print("msgpack client....")
-		socket = spec.get('socket', 'mock.socket')
-		print(f"opening socket {socket}")
-		try_methods = spec.get('try_methods', [])
-		MsgPackTestClient(socket, try_methods).try_all()
+		cli = MsgPackTestClient(spec)
+		cli.start_target()
+		cli.try_all()
+		cli.close()
 
 
 # ==== command start ====
@@ -165,7 +212,13 @@ def main():
 	# parser.add_argument('-n', '--name', metavar='<name>', help='An optional parameter')
 	# parser.add_argument('-e', '--extra', action='store_true', help='This Value is False by default')
 	parser.add_argument('-s', '--spec', help="JSON file with test specification (default /dev/stdin)")
+	parser.add_argument('-v', '--verbose', action='count', default=0)
 	args = parser.parse_args()
+
+	if args.verbose >= 2:
+		logging.getLogger().setLevel(logging.DEBUG)
+	elif args.verbose >= 1:
+		logging.getLogger().setLevel(logging.INFO)
 
 	spec = load_spec(args.spec)
 	route_protocol(spec)
